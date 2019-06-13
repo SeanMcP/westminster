5910bb87420c3a137d61a3389a33927f8c95ee84
Sean McPherson <mail@seanmcp.com>
2019-06-13 06:51:49 -0400

    Add hot reloading with two commands
    There has to be a better way.

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,13 @@ import express from 'express'
 const PORT = 1646
 
 const app = express();
-app.get('/', (req, res) => res.send('Hello from Westminster'))
+app.get('/', (req, res) => res.send('Howdy from Westminster'))
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+const server = app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+
+declare const module: any;
+
+if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => server.close());
+}
--- webpack.config.js
+++ webpack.config.js
@@ -6,16 +6,17 @@ module.exports = {
   mode: "development",
   entry: "./src/app.ts",
   output: {
-    filename: "bundle.js",
+    filename: "app.js",
     path: path.join(__dirname + "/dist")
   },
+  watch: true,
   devtool: "source-map",
   resolve: {
     extensions: [".ts", ".js", ".json"]
   },
   module: {
     rules: [
-      { test: /\.ts/, loader: "awesome-typescript-loader" },
+      { test: /\.ts/, loader: "awesome-typescript-loader", exclude: "/node_modules/" },
       { enforce: "pre", test: /\.js/, loader: "source-map-loader" }
     ]
   },
